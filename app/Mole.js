86f8074c8f618a90e7561e4d640a4b886e1c0bfc
jojeco <jojeco717@gmail.com>
2024-02-04 17:06:25 -0700

    Works_V1.2
    The avocado is the only thing that can be tapped and increase score

--- app/Mole.js
+++ app/Mole.js
@@ -3,14 +3,18 @@ import { View, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import moleImage from '../assets/images/mole.png';
 
 
-    const Mole = ({ isVisible, onPress }) => {
-        return (
-            <TouchableOpacity onPress={onPress} style={styles.moleContainer}>
-                {isVisible && <Image source={moleImage} style={styles.mole} />}
-            </TouchableOpacity>
-        );
-    };
+const Mole = ({ isVisible, onPress }) => {
+    return isVisible ? (
+        <TouchableOpacity onPress={onPress} style={styles.moleContainer}>
+            <Image source={moleImage} style={styles.mole} />
+        </TouchableOpacity>
+    ) : (
+        <View style={styles.moleContainer} />
+    );
     
+};
+
+
 
 const styles = StyleSheet.create({
     moleContainer: {
@@ -20,9 +24,8 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     mole: {
-        width: 80,   // Adjust size as needed
-        height: 80,  // Adjust size as needed
-        // Add any additional styling for the image
+        width: 100,   // Adjust size as needed
+        height: 100,  // Adjust size as needed
     },
 });
 export default Mole;
